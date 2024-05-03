d98102b66a406845018650b9d82c317c05cf2c11
YusifAlceferov <alcafarovyusif1122@gmail.com>
2024-05-03 18:31:08 +0400

    first commit

---
+++ task.py
@@ -0,0 +1,29 @@
+# ------------------------------
+# task1
+# list1 = sorted([int(input(f"{i+1}. ədədi daxil edin: "))
+#  for i in range(6)])
+# print(list1)
+# -------------------------------------
+# task2
+# string = input("soz daxil edin:")
+# ters_yazi = ''.join(sorted(string))
+# print(ters_yazi)
+#-------------+++++++++++-----------------
+  #task3
+# n=0
+# cehdsayi=0
+# while n!=13:
+#   n=int(input("eded daxil edin:"))
+#   cehdsayi+=1
+# print (f"doğru texmin! {cehdsayi}")
+#-------------------------------
+#  task4
+# list=[]
+# for num in range(1, 101):
+#     if num > 1:
+#         for i in range(2, num):
+#             if (num % i) == 0:
+#                 break
+#         else:
+#          list.append(num)
+# print(list)
